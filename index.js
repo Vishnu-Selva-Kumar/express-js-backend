743c138ec060156ac0cc3fd075742afc6ccb61f8
Vishnu-Selva-Kumar <vis.selva.git@gmail.com>
2026-09-07 00:46:06 +0530

    feat(web): add welcome message to root endpoint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello from Express inside Docker!');
+  res.send('<h1>Welcome to Express.js!</h1>');
 });
 
 app.use(webRoutes);
